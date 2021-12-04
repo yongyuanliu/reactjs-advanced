3a1e0edd22d7d55fbfa095c5c01947308389a376
Yongyuan Liu <stormbluer@outlook.com>
2021-12-04 17:58:39 +0800

    nested context example

--- src/context/ThemeApp.js
+++ src/context/ThemeApp.js
@@ -1,51 +1,38 @@
 import React from "react";
-import ThemedButton from "./theme/theme-button";
-import { ThemeContext, themes } from "./theme/theme-context";
-import SigninButton from "./user/signin-button";
-import SignoutButton from "./user/signout-button";
-import { AuthContext, fakeAuth } from "./user/user-context";
+import ThemeTogglerButton from "./theme-nested/theme-button";
+import { ThemeContext, themes } from "./theme-nested/theme-context";
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.toggleTheme = () => {
+            console.log(this.state.theme);
+            this.setState(state => ({
+                theme: state.theme === themes.dark
+                    ? themes.light
+                    : themes.dark
+            }));
+        }
+
         this.state = {
             theme: themes.light,
-            user: fakeAuth.user,
+            toggleTheme: this.toggleTheme,
         }
-        this.toggleTheme = () => {
-            console.log(this.state);
-            this.setState(state => ({
-                theme: state.theme === themes.dark ? themes.light : themes.dark
-            }))
-        }
-        console.log(fakeAuth.user);
     }
     render() {
         return (
-            <div>
-                <ThemeContext.Provider value={this.state.theme}>
-                    <Toolbar changeTheme={this.toggleTheme} />
-                </ThemeContext.Provider>
-                <AuthContext.Provider value={this.state.user}>
-                    {
-                        this.state.user === null ? <SigninButton name="Yongyuan" callback={() => {
-                            this.setState({ user: fakeAuth.user });
-                        }}>登录</SigninButton>
-                            : <SignoutButton callback={() => {
-                                this.setState({ user: fakeAuth.user });
-                            }}>登出</SignoutButton>
-                    }
-
-                </AuthContext.Provider>
-            </div>
-        )
+            <ThemeContext.Provider value={this.state}>
+                <Context />
+            </ThemeContext.Provider>
+        );
     }
 }
 
-function Toolbar(props) {
+function Context() {
     return (
-        <ThemedButton onClick={props.changeTheme}>
-            Change Theme
-        </ThemedButton>
+        <div>
+            <ThemeTogglerButton />
+        </div>
     );
 }
